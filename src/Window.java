1917b4ebd8ff59417e23d11f33adb7a3786598c8
Ivan <zvery04@gmail.com>
2020-07-22 23:00:14 +0700

    Postavil kameru, dobavil kvadrat, dvigaetsya

--- src/Window.java
+++ src/Window.java
@@ -13,6 +13,7 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 public class Window{
     private long window;
+    private int x = 50, y = 50;
 
     public void run(){
         System.out.println("Start");
@@ -42,7 +43,7 @@ public class Window{
             throw new RuntimeException("Failed to create the GLFW window");
 
         glfwSetKeyCallback(window, (window, key, scancode, action, mods) ->{
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
                 glfwSetWindowShouldClose(window, true);
         });
 
@@ -63,9 +64,29 @@ public class Window{
         glClearColor(0.4f, 0.4f, 0.2f, 0.0f);
         while (!glfwWindowShouldClose(window)){
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(0, 640, 480, 0, 1, -1);
+            glMatrixMode(GL_MODELVIEW);
+
+            if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+                x+=1;
+            if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+                x-=1;
+            if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+                y-=1;
+            if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+                y+=1;
+
+            glColor3d(0.1, 0.8, 0.1);
+            glBegin(GL_QUADS);
+            glVertex2f(x, y);
+            glVertex2f(x + 50, y);
+            glVertex2f(x + 50, y + 50);
+            glVertex2f(x, y + 50);
+            glEnd();
 
             glfwSwapBuffers(window);
-
             glfwPollEvents();
         }
     }
