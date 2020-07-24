6fcb1edcdf3b2a140d9abed3d96318af709f27c3
Ivan <zvery04@gmail.com>
2020-07-25 03:03:07 +0700

    Texture syka rabotaet, ura, daje ne spolzaet

--- src/Window.java
+++ src/Window.java
@@ -3,6 +3,10 @@ import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.*;
 
 import static org.lwjgl.glfw.Callbacks.*;
@@ -10,10 +14,14 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.stb.STBImage.*;
+
+import javax.imageio.IIOImage;
 
 public class Window{
     private long window;
     private int x = 50, y = 50;
+    int idBox;
 
     public void run(){
         System.out.println("Start");
@@ -57,17 +65,24 @@ public class Window{
         glfwMakeContextCurrent(window);
         glfwSwapInterval(1);
         glfwShowWindow(window);
+
+        GL.createCapabilities();
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, 640, 480, 0, 1, -1);
+        glMatrixMode(GL_MODELVIEW);
+        glEnable(GL_TEXTURE_2D);
+        idBox = Texture.loadTexture("box");
+
+
     }
 
     private void loop(){
-        GL.createCapabilities();
+
         glClearColor(0.4f, 0.4f, 0.2f, 0.0f);
+
         while (!glfwWindowShouldClose(window)){
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            glMatrixMode(GL_PROJECTION);
-            glLoadIdentity();
-            glOrtho(0, 640, 480, 0, 1, -1);
-            glMatrixMode(GL_MODELVIEW);
 
             if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
                 x+=1;
@@ -78,11 +93,17 @@ public class Window{
             if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
                 y+=1;
 
+            glBindTexture(GL_TEXTURE_2D, idBox);
+
             glColor3d(0.1, 0.8, 0.1);
             glBegin(GL_QUADS);
+            glTexCoord2d(0, 0);
             glVertex2f(x, y);
+            glTexCoord2d(1, 0);
             glVertex2f(x + 50, y);
+            glTexCoord2d(1, 1);
             glVertex2f(x + 50, y + 50);
+            glTexCoord2d(0, 1);
             glVertex2f(x, y + 50);
             glEnd();
 
