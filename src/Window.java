0cbe951b3cbd9c8daa50c66bc70a23570b9dd9b0
Ivan <zvery04@gmail.com>
2020-07-29 14:49:46 +0700

    player dobavil

--- src/Window.java
+++ src/Window.java
@@ -13,7 +13,7 @@ import static org.lwjgl.system.MemoryUtil.*;
 public class Window{
     private long window;
     private int x = 50, y = 50, x1 = 100, y1 = 100;
-    int idBox, idPlayer;
+    int idBox, idPlayerStand, idPlayerRight, idPlayerLeft, idPlayerUp, idPlayerDown;
 
     public void run(){
         System.out.println("Start");
@@ -67,41 +67,56 @@ public class Window{
         glBlendFunc(GL_SRC_ALPHA,GL_ONE);
         glEnable(GL_BLEND);
         idBox = Texture.loadTexture("box");
-        idPlayer = Texture.loadTexture("player_05");
-
+        idPlayerStand = Texture.loadTexture("player_stand");
+        idPlayerRight = Texture.loadTexture("player_right");
+        idPlayerLeft = Texture.loadTexture("player_left");
+        idPlayerUp = Texture.loadTexture("player_up");
+        idPlayerDown = Texture.loadTexture("player_down");
     }
 
     private void loop(){
 
         glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
+        String isMovement;
 
         while (!glfwWindowShouldClose(window)){
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            isMovement = "stand";
+            if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
+                isMovement = "right";
+                x1+=1;
+            }
+            if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
+                isMovement = "left";
+                x1-=1;
+            }
+            if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
+                isMovement = "up";
+                y1-=1;
+            }
+            if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
+                isMovement = "down";
+                y1+=1;
+            }
 
-            if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-                x+=1;
-            if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-                x-=1;
-            if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-                y-=1;
-            if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-                y+=1;
+            switch (isMovement){
+                case "stand":
+                    glBindTexture(GL_TEXTURE_2D, idPlayerStand);
+                    break;
+                case "right":
+                    glBindTexture(GL_TEXTURE_2D, idPlayerRight);
+                    break;
+                case "left":
+                    glBindTexture(GL_TEXTURE_2D, idPlayerLeft);
+                    break;
+                case "up":
+                    glBindTexture(GL_TEXTURE_2D, idPlayerUp);
+                    break;
+                case "down":
+                    glBindTexture(GL_TEXTURE_2D, idPlayerDown);
+                    break;
+            }
 
-            glBindTexture(GL_TEXTURE_2D, idBox);
-
-
-            glBegin(GL_QUADS);
-            glTexCoord2d(0, 0);
-            glVertex2f(x, y);
-            glTexCoord2d(1, 0);
-            glVertex2f(x + 50, y);
-            glTexCoord2d(1, 1);
-            glVertex2f(x + 50, y + 50);
-            glTexCoord2d(0, 1);
-            glVertex2f(x, y + 50);
-            glEnd();
-
-            glBindTexture(GL_TEXTURE_2D, idPlayer);
             glBegin(GL_QUADS);
             glTexCoord2d(0, 0);
             glVertex2f(x1, y1);
@@ -111,6 +126,18 @@ public class Window{
             glVertex2f(x1 + 50, y1 + 50);
             glTexCoord2d(0, 1);
             glVertex2f(x1, y1 + 50);
+            glEnd();
+
+            glBindTexture(GL_TEXTURE_2D, idBox);
+            glBegin(GL_QUADS);
+            glTexCoord2d(0, 0);
+            glVertex2f(x, y);
+            glTexCoord2d(1, 0);
+            glVertex2f(x + 50, y);
+            glTexCoord2d(1, 1);
+            glVertex2f(x + 50, y + 50);
+            glTexCoord2d(0, 1);
+            glVertex2f(x, y + 50);
             glEnd();
 
             glfwSwapBuffers(window);
